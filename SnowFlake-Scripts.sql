49557b020525d1f80dd7ec5823530e79f26f41e5
Pouya <pouya.ataei.7@gmail.com>
2023-01-05 17:42:29 +1300

    testing the loading of JSON right into columns, testing with datetime

--- SnowFlake-Scripts.sql
+++ SnowFlake-Scripts.sql
@@ -21,11 +21,13 @@ CREATE OR REPLACE STAGE CSV_DATA_STAGE file_format= csv_format;
 put file:///home/pouya/Desktop/projects/Data-Architecture-Yelp/datasets/usw00023169-las-vegas-mccarran-intl-ap-precipitation-inch.csv @CSV_DATA_STAGE auto_compress=true;
 put file:///home/pouya/Desktop/projects/Data-Architecture-Yelp/datasets/usw00023169-temperature-degreef.csv @CSV_DATA_STAGE auto_compress=true;
 
--- Create a table for Climate temperature degrees
+-- Creating a table for Climate temperature degrees
 CREATE TABLE ClimateTemperatureDegrees (date number,min float,max float, normal_min float, normal_max float);
+
+-- Creating a table for Climate preciptations
 CREATE TABLE ClimatePrecipitation (date number,precipitation varchar,precipitation_normal float);
 
--- Copying files Climate datasets from CSV staging area to corresponding tables
+-- Copying Climate datasets from CSV staging area to corresponding tables
 COPY INTO ClimateTemperatureDegrees FROM @csv_data_stage/usw00023169-temperature-degreef.csv.gz file_format=csv_format; 
 COPY INTO ClimatePrecipitation FROM @csv_data_stage/usw00023169-las-vegas-mccarran-intl-ap-precipitation-inch.csv.gz file_format=csv_format;
 
@@ -54,5 +56,29 @@ put file:///home/pouya/Desktop/projects/Data-Architecture-Yelp/datasets/yelp_aca
 put file:///home/pouya/Desktop/projects/Data-Architecture-Yelp/datasets/yelp_academic_dataset_tip.json @JSON_DATA_STAGE auto_compress=true;
 
 
--- Create a table for Yelp customer tips 
-CREATE TABLE userTips()
+-- Creating a table for Yelp customer tips 
+CREATE TABLE userTips(tip variant);
+
+-- Creating a table for Yelp COVID features dataset
+CREATE TABLE covidFeatures(covidFeature variant);
+
+-- Creating a table for Yelp customer check ins
+CREATE TABLE checkins(checkIn variant);
+
+-- Creating a table for Yelp businesses dataset
+CREATE TABLE businesses(business variant);
+
+-- Creating a table for Yelp customer reviews
+CREATE TABLE reviews(review variant);
+
+-- Creating a table for Yelp users
+CREATE TABLE users(user variant);
+
+-- Copying Yelp datasets from JSON staging area to corresponding tables
+COPY INTO userTips FROM @json_data_stage/yelp_academic_dataset_tip.json.gz  file_format=json_format; 
+COPY INTO covidFeatures FROM @json_data_stage/yelp_academic_dataset_tip.json.gz  file_format=json_format; 
+COPY INTO userTips FROM @json_data_stage/yelp_academic_dataset_tip.json.gz  file_format=json_format; 
+COPY INTO userTips FROM @json_data_stage/yelp_academic_dataset_tip.json.gz  file_format=json_format; 
+
+COPY INTO jsonTest (user_id, business_id, text, date, compliment_count) 
+from (select $1:user_id::varchar, $1:business_id::varchar, $1:text::varchar, $1:date::timestamp_ltz, $1:compliment_count::number from @json_data_stage/yelp_academic_dataset_tip.json.gz t);
